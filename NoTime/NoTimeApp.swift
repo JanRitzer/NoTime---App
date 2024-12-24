cb2a109db8ddb26b8de7554fa746cbd28ed45006
Jan Ritzer <jan.ritzer@web.de>
2024-12-24 23:24:58 +0100

    Initial Commit

---
+++ NoTime/NoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoTimeApp.swift
+//  NoTime
+//
+//  Created by Jan Ritzer on 24.12.24.
+//
+
+import SwiftUI
+
+@main
+struct NoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoTimeTests/NoTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoTimeTests.swift
+//  NoTimeTests
+//
+//  Created by Jan Ritzer on 24.12.24.
+//
+
+import Testing
+@testable import NoTime
+
+struct NoTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
